1c1280cf5e49bdd5cba3fcec319b5e670014d29e
Abigail Turner <73142622+Buzbuz9@users.noreply.github.com>
2022-03-14 11:47:53 -0400

    Create Shared Graph Class
    This is the graph class I thought we could all share and extend

---
+++ Graph.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class Graph extends Node{
+    LinkedList<Node> nodes; //adj list
+    ArrayList<LinkedList<Node>> adjList; //other adj list for bfs
+    int[][] matrix;//matrix to traverse throuhg
+    Node[] nodeMatrix; //to retrieve the nodes from
+    Set<Node> allNodes; //set of all nodes in the graph
+    
+    public Graph(LinkedList<Node> adj, Set<Node> s){
+      nodes = adj;
+      allNodes = s;
+    }
+    public Graph(int[][] m, Node[] n)
+    {
+       matrix = m;
+       nodeMatrix = n;
+       
+    }
+    public Graph(ArrayList<LinkedList<Node>> a){
+      adjList = a;
+    }
+    
+    public ArrayList<LinkedList<Node>> getAdjList(){
+      return adjList;
+    }
+    
+    
+    public Node[] getNodeArray(){
+      return nodeMatrix;
+    }
+    
+    public int[][] getMatrix(){
+      return matrix;
+    }
+    
+    public boolean isListEmpty(){
+      return true;
+    }
+    
+    public boolean isMatrixEmpty(){
+       return true;
+
+    }
+    
+    public boolean isSetEmpty(){
+     return true;
+
+    }
+    
+    public void addNode(Node a){
+      allNodes.add(a);
+    }
+    
+}
