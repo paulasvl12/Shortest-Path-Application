41dcc53305e1a6a4f73eaefef8da92681ee86fb4
Abigail Turner <abigailreannturner@gmail.com>
2022-03-29 13:38:58 -0400

    Update GUIWindow.java

--- GUIWindow.java
+++ GUIWindow.java
@@ -240,15 +240,14 @@ public class GUIWindow extends JPanel {
                 @Override
                 public void mousePressed(MouseEvent e) {
                 //right click
-                if(e.isMetaDown()){
-                  //get the state of the node and store it
-                  //if it was start or end reset the boolean for selected
-                  //counter--;
-                  
-                  //reset the state of associated node to ""
-                  //color the cell white again/remove the color
-                  
-                }
+                   if(e.isMetaDown()){
+                     //get the state of the node and store it
+                     //if it was start or end reset the boolean for selected
+                     //counter--;
+                     
+                     //reset the state of associated node to ""
+                     //color the cell white again/remove the color
+                   }
                 
                 }
             });
