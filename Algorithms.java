7f6e4e4b1f49a6b30d1c62e54e785d175f71bcc3
Abigail Turner <abigailreannturner@gmail.com>
2022-03-29 20:34:33 -0400

    Making nodes

--- Algorithms.java
+++ Algorithms.java
@@ -67,6 +67,25 @@ public class mat{
          Node n0 = new Node("",0, 0, 0);
          Node n1 = new Node("",1, 0, 1);
          Node n2 = new Node("",2, 0, 2);
+         Node n3 = new Node("",3, 0, 3);
+         Node n4 = new Node("",4, 0, 4);
+         Node n5 = new Node("",5, 0, 5);
+         Node n6 = new Node("",6, 0, 6);
+         Node n7 = new Node("",7, 1, 0);
+         Node n8 = new Node("",8, 1, 1);
+         Node n9 = new Node("",9, 1, 2);
+         Node n10 = new Node("",10, 1, 3);
+         Node n11 = new Node("",11, 1, 3);
+         Node n12 = new Node("",12, 1, 3);
+         Node n13 = new Node("",13, 1, 3);
+         Node n14 = new Node("",14, 2, 0);
+         Node n15 = new Node("",15, 2, 1);
+         Node n16 = new Node("",16, 2, 2);
+         Node n17 = new Node("",17, 2, 3);
+         Node n18 = new Node("",18, 2, 4);
+         Node n19 = new Node("",19, 2, 5);
+         Node n20 = new Node("",20, 2, 6);
+
          
          array[0][0] = 0;
          array[1][0] = 1;
