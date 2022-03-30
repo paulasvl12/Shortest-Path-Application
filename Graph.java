ab20ece3a40ee8cb3dfb1920e686b5726b29633e
mariashiferaw <66911924+mariashiferaw@users.noreply.github.com>
2022-03-30 11:00:55 -0400

    Add files via upload

--- Graph.java
+++ Graph.java
@@ -1,54 +1,22 @@
-import java.util.*;
-public class Graph extends Node{
-    LinkedList<Node> nodes; //adj list
-    ArrayList<LinkedList<Node>> adjList; //other adj list for bfs
-    int[][] matrix;//matrix to traverse throuhg
-    Node[] nodeMatrix; //to retrieve the nodes from
-    Set<Node> allNodes; //set of all nodes in the graph
-    
-    public Graph(LinkedList<Node> adj, Set<Node> s){
-      nodes = adj;
-      allNodes = s;
-    }
-    public Graph(int[][] m, Node[] n)
-    {
-       matrix = m;
-       nodeMatrix = n;
-       
-    }
-    public Graph(ArrayList<LinkedList<Node>> a){
-      adjList = a;
-    }
-    
-    public ArrayList<LinkedList<Node>> getAdjList(){
-      return adjList;
-    }
-    
-    
-    public Node[] getNodeArray(){
-      return nodeMatrix;
-    }
-    
-    public int[][] getMatrix(){
-      return matrix;
-    }
-    
-    public boolean isListEmpty(){
-      return true;
-    }
-    
-    public boolean isMatrixEmpty(){
-       return true;
+package com.example.pathfinding;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Graph {
+
+
+    public static class Edge {
+        Edge(int weight, Node node) {
+            this.weight = weight;
+            this.node = node;
+        }
+
+        public int weight;
+        public Node node;
 
     }
-    
-    public boolean isSetEmpty(){
-     return true;
 
-    }
-    
-    public void addNode(Node a){
-      allNodes.add(a);
-    }
-    
+
 }
--- Node.java
+++ Node.java
@@ -1,80 +1,56 @@
-import java.util.*;
-public class Node
-       {
-      	  private boolean visited;   
-           private Node prev;   //used in bfs to trace the path at the end
-           private Node next;  
-           private String state; //start end or obstacle "Start" "End" "Obs" .
-           private String gridCoor; //how we will connect it to our visual represenation
-           private Integer distance; //used for dijstar and other weighted graphs
-           private int id; //number associated with the matrix?
-            
-      	  public Node()
-      	  {
-      	     visited = false;  //no nodes have been visited yet
-      	     prev  = null;      
-              next = null;
-              state = "";
-              gridCoor = "";
-              distance = Integer.MAX_VALUE; //sets edges to ininifty
-      	  }
-           
-           public Node(String type, String coor, int id)
-           {
-              visited = false;  //no nodes have been visited yet
-      	     prev  = null;      
-              next = null;
-              state = type;
-              gridCoor = coor;
-              this.id = id;
-              distance = Integer.MAX_VALUE; //sets edges to ininifty
-           }
-           public String getGridCoor(){
-            return gridCoor;
-           }
-           public void setGridCoor(String n){
-            gridCoor = n;
-           }
-           
-           public int getID(){
-            return id;
-           }
-           
-           public void setID(int n){
-            id = n;
-           }
-           
-           public boolean getVisited(){
-            return visited;
-           }
-           
-           public void setVisited(boolean b){
-            visited = b;
-           }
-           public String getState(){
-            return state;
-           }
-           public void setState(String s){
-            state = s;
-           }
-           public Node getNext(){
-            return next;
-           }
-           public void setNext(Node n){
-            next = n;
-           }
-           
-           public Node getPrev(){
-            return prev;
-           }
-           public void setPrev(Node n){
-            prev = n;
-           }
-           
-           public boolean equals(Node n){
-            if(this.visited == n.visited && this.state.equals(n.state) && this.gridCoor.equals(n.gridCoor))
-               return true;
-            else
-               return false;
-           }
+package com.example.pathfinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node implements Comparable<Node> {
+    // Id for readability of result purposes
+    private static int idCounter = 0;
+    public int id;
+
+    // Parent in the path
+    public Node parent = null;
+
+    public List<Edge> neighbors;
+
+    // Evaluation functions
+    public double f = Double.MAX_VALUE;
+    public double g = Double.MAX_VALUE;
+    // Hardcoded heuristic
+    public double h;
+
+    Node(double h) {
+        this.h = h;
+        this.id = idCounter++;
+        this.neighbors = new ArrayList<>();
+    }
+
+    @Override
+    public int compareTo(Node n) {
+        return Double.compare(this.f, n.f);
+    }
+
+    public static class Edge {
+        Edge(int weight, Node node) {
+            this.weight = weight;
+            this.node = node;
+        }
+
+        public int weight;
+        public Node node;
+    }
+
+    public void addBranch(int weight, Node node) {
+        Edge newEdge = new Edge(weight, node);
+        neighbors.add(newEdge);
+    }
+
+    public double calculateHeuristic(Node target) {
+        return this.h;
+    }
 }
+
+
+
+    
+    
