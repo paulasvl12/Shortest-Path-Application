2cfa3588e0deb09994eb6159110c76bad482c4ea
mariashiferaw <66911924+mariashiferaw@users.noreply.github.com>
2022-03-30 14:13:23 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -2,19 +2,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Graph {
+public class Graph extends Node {
+    public ArrayList<Edge> neighbors;
 
-
-    public static class Edge {
-        Edge(int weight, Node node) {
-            this.weight = weight;
-            this.node = node;
-        }
-
-        public int weight;
-        public Node node;
+    public Edge(int weight, Node node) {
+        this.weight = weight;
+        this.node = node;
+        this.neighbors = new ArrayList<>();
 
     }
 
+    public int weight;
+    public Node node;
 
 }
+
+
+
