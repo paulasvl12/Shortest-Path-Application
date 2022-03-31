4829ac54a4b4da74cba19e84097a86b257305e52
mariashiferaw <66911924+mariashiferaw@users.noreply.github.com>
2022-03-30 23:06:06 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Graph extends Node {
+<<<<<<< Updated upstream
 
    public Graph(int h, Node n) {
           
@@ -30,3 +31,42 @@ public class Graph extends Node {
 
 
 
+=======
+   int[][] adjMatrix;
+   ArrayList<Edge> edgeList;
+   
+   
+   public Graph(int[][] g) {
+      adjMatrix = g;
+   }
+   
+   public int[][] getAdjMatrix(){
+      return this.adjMatrix;
+   }
+   
+   ArrayList<Edge> getEdges(){
+      ArrayList<Edge> edgeList = new ArrayList<Edge>();
+      
+      for(int start = 0; start < this.adjMatrix.length; start++){
+         for(int end = start; end < this.adjMatrix[start].length; end++){
+            if(this.adjMatrix[start][end] != 0){
+               edgeList.add(new Edge(start, end, this.adjMatrix[start][end]));
+            }
+         }
+      }
+      
+      return edgeList;
+   }
+   
+   class Edge {
+   int source, dest, weight;
+   
+   public Edge(int source, int dest, int weight){
+      this.source = source;
+      this.dest = dest;
+      this.weight = weight;
+   }
+   
+   }
+}
+>>>>>>> Stashed changes
