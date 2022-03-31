876fd78634bd7f9f52b3327c6e5d42b3f8a3ee96
mariashiferaw <66911924+mariashiferaw@users.noreply.github.com>
2022-03-30 23:42:33 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -2,42 +2,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+public class Graph {
 public class Graph extends Node {
-   int[][] adjMatrix;
-   ArrayList<Edge> edgeList;
-   
-   
-   public Graph(int[][] g) {
-      adjMatrix = g;
-   }
-   
-   public int[][] getAdjMatrix(){
-      return this.adjMatrix;
-   }
-   
-   ArrayList<Edge> getEdges(){
-      ArrayList<Edge> edgeList = new ArrayList<Edge>();
-      
-      for(int start = 0; start < this.adjMatrix.length; start++){
-         for(int end = start; end < this.adjMatrix[start].length; end++){
-            if(this.adjMatrix[start][end] != 0){
-               edgeList.add(new Edge(start, end, this.adjMatrix[start][end]));
-            }
-         }
-      }
-      
-      return edgeList;
-   }
-   
-   class Edge {
-   int source, dest, weight;
-   
-   public Edge(int source, int dest, int weight){
-      this.source = source;
-      this.dest = dest;
-      this.weight = weight;
-   }
-   
-   }
-}
+    public ArrayList<Edge> neighbors;
 
+
+    public static class Edge {
+        Edge(int weight, Node node) {
+            this.weight = weight;
+            this.node = node;
+        }
+
+        public int weight;
+        public Node node;
+    public Edge(int weight, Node node) {
+        this.weight = weight;
+        this.node = node;
+        this.neighbors = new ArrayList<>();
+
+    }
+
+    public int weight;
+    public Node node;
+
+}
