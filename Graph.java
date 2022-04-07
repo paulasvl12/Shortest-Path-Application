971337eedeaf1222d280a5d1bb9903eb003e55c5
Paula Viloria <paulasvl12@gmail.com>
2022-04-07 11:17:19 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -10,9 +10,157 @@ public class Graph extends Node {
     ArrayList<Node> nodeList; // Added for BellmanFord algorithm
     ArrayList<Edge> edgeList; // Added for BellmanFord algorithm
 
-   
-     
-     // Returns graph represented by its edges
+    //Dijkstra
+    public static ArrayList<Integer> shortestPath = new ArrayList<>(); //dijkstra
+    public Map<Integer, Node> graph = new HashMap<>(); // mapping of Node ids to Node objects, built from a set of Edges
+
+    public static class Node implements Comparable<Node>{
+        public int id;
+        public int distance = Integer.MAX_VALUE; // MAX_VALUE assumed to be infinity
+        public Node previous = null;
+        public final Map<Node, Integer> neighbours = new HashMap<>();
+
+        public Node(int id)
+        {
+            this.id = id;
+        }
+
+        private void printPath()
+        {
+            if (this == this.previous)
+            {
+                System.out.printf("%s", this.id);
+            }
+            else if (this.previous == null)
+            {
+                System.out.printf("%s(unreached)", this.id);
+            }
+            else
+            {
+                this.previous.printPath();
+                System.out.printf(" -> %s(%d)", this.id, this.distance);
+            }
+        }
+
+        public int compareTo(Node other)
+
+        {
+
+            if (distance == other.distance) {
+                if (id == other.id) {
+                    return 0;
+                } else if(id < other.id){
+                    return -1;
+                } else if(id > other.id){
+                    return 1;
+                }
+                //    return id.compareTo((Integer)other.id); }
+            }
+            return Integer.compare(distance, other.distance);
+        }
+    }
+    public Graph(Edge[] edges) {
+        graph = new HashMap<>(edges.length);
+
+        //one pass to find all vertices
+        for (Edge e : edges) {
+            if (!graph.containsKey(e.source)) graph.put(e.source, new Node(e.source));
+            if (!graph.containsKey(e.dest)) graph.put(e.dest, new Node(e.dest));
+        }
+
+        //another pass to set neighbouring vertices
+        for (Edge e : edges) {
+            graph.get(e.source).neighbours.put(graph.get(e.dest), e.distance);
+            //graph.get(e.dest).neighbours.put(graph.get(e.source), e.distanceance); // also do this for an undirected graph
+        }
+    }
+    public void printEdges(){
+        for ( Integer key : graph.keySet() ) {
+            System.out.println( key );
+        }
+    }
+
+    public void printPath(int endid) {
+        if (!graph.containsKey(endid)) {
+            System.err.printf("Graph doesn't contain end Node \"%s\"\n", endid);
+            return;
+        }
+
+        graph.get(endid).printPath();
+        System.out.println();
+    }
+
+    public void dijkstra(int startid) {
+        if (!graph.containsKey(startid)) {
+            System.err.printf("Graph doesn't contain start Node \"%s\"\n", startid);
+            return;
+        }
+        final Node source = graph.get(startid);
+        NavigableSet<Node> q = new TreeSet<>();
+
+        // set-up vertices
+        for (Node v : graph.values()) {
+            v.previous = v == source ? source : null;
+            v.distance = v == source ? 0 : Integer.MAX_VALUE;
+            q.add(v);
+        }
+
+        dijkstra(q);
+    }
+
+    /**
+     * Implementation of dijkstra's algorithm using a binary heap.
+     */
+
+    private void dijkstra(NavigableSet<Node> q) {
+        Node u, v;
+        while (!q.isEmpty()) {
+
+            u = q.pollFirst(); // Node with shortest distanceance (first iteration will return source)
+            if (u.distance == Integer.MAX_VALUE)
+                break; // we can ignore u (and any other remaining vertices) since they are unreachable
+
+            //look at distanceances to each neighbour
+            for (Map.Entry<Node, Integer> a : u.neighbours.entrySet()) {
+                v = a.getKey(); //the neighbour in this iteration
+
+                final int alternatedistance = u.distance + a.getValue();
+                if (alternatedistance < v.distance) { // shorter path to neighbour found
+                    q.remove(v);
+                    v.distance = alternatedistance;
+                    v.previous = u;
+                    q.add(v);
+                }
+            }
+        }
+    }
+    public static class Edge extends Node {
+        int source, dest, weight;
+
+        public Edge(int source, int dest, int weight)
+        {
+            super(source);
+            this.source = source;
+            this.dest = dest;
+            this.weight = weight;
+        }
+        // Returns graph represented by its edges
+
+
+        // A class to store a graph edge
+
+
+
+        // Overrides the default toString() method so prints out in a readable format
+        public String toString(){
+            return "("+ this.source+ ", "+this.dest + ", "+this.weight + ")";
+        }
+    }
+    //end of dijkstra
+
+
+
+    // Returns graph represented by its edges
     public ArrayList<Edge> getEdges(){
 
       ArrayList<Edge> edgeList = new ArrayList<Edge>();
